630ed28ef340d4eb90ac0927034d269a6ef578a9
Miguel Albert <miguelalbert38@gmail.com>
2024-11-30 09:59:37 -0300

    Home page complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from "./style/theme/default"
-import { GlobalStyle } from "./style/global"
+import { GlobalStyle } from "./style/global"  
 import { Router } from "./Router";
 
 export function App() {
